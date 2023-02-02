92d09251a6390ee4b1d136346f01de4feb583783
briguetjo <briguetjo@gmail.com>
2023-02-02 22:08:39 +0100

    mark a param as required

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def init():
     model_name = os.getenv("MODEL_NAME")
     model = whisper.load_model(model_name, device="cuda", in_memory=True, fp16=True)
 
-def _parse_arg(args : str, data : dict, default = None):
+def _parse_arg(args : str, data : dict, default = None, required = False):
     arg = data.get(args, None)
     if arg == None:
-        if default is None:
+        if required:
             raise Exception(f"Missing required argument: {args}")
         else:
             return default
@@ -27,11 +27,12 @@ def inference(model_inputs:dict) -> dict:
 
     # Parse out your arguments
     try:
-        BytesString = _parse_arg("base64String", model_inputs)
+        BytesString = _parse_arg("base64String", model_inputs, required=True)
         beam_size = _parse_arg("beam_size", model_inputs, None)
         best_of = _parse_arg("best_of", model_inputs, 5)
         audio_type = _parse_arg("audio_type", model_inputs, "opus")
         temperature = _parse_arg("temperature", model_inputs, (0.0, 0.2, 0.7))
+        initial_prompt = _parse_arg("initial_prompt", model_inputs, None)
         if audio_type not in ["opus", "wav", "flac", "mp3", "m4a"]:
             raise Exception(f"Invalid audio_type: {audio_type}")
 
@@ -51,6 +52,7 @@ def inference(model_inputs:dict) -> dict:
         temperature=temperature,
         beam_size=beam_size,
         best_of=best_of,
+        initial_prompt=initial_prompt,
         )
     os.remove(tmp_file)
     # Return the results as a dictionary
