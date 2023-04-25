637e68798e748c3450b3d44fbaed72d464dded1f
yamete <eimantaslimba7+DevastMod@gmail.com>
2023-04-25 17:10:04 +0300

    yt download

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import whisper
 import os
+import subprocess
 import base64
 from io import BytesIO
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -10,7 +12,8 @@ def init():
     model_name = os.getenv("MODEL_NAME")
     model = whisper.load_model(model_name, device="cuda", in_memory=True, fp16=True)
 
-def _parse_arg(args : str, data : dict, default = None, required = False):
+
+def _parse_arg(args: str, data: dict, default=None, required=False):
     arg = data.get(args, None)
     if arg == None:
         if required:
@@ -20,37 +23,56 @@ def _parse_arg(args : str, data : dict, default = None, required = False):
 
     return arg
 
+
+def downloadYTaudio(url, start_time, end_time, audio_file):
+    # Download audio file
+    audio_cmd = f'yt-dlp -f "bestaudio[ext=m4a]" --external-downloader ffmpeg --external-downloader-args "ffmpeg_i:-ss {start_time} -to {end_time}" -o "{audio_file}" "{url}"'
+    audio_output = subprocess.run(audio_cmd, shell=True, capture_output=True, text=True)
+
+    return (
+        audio_output.returncode,
+        audio_output.stderr,
+    )
+
+
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
-    # Parse out your arguments
-    try:
-        BytesString = _parse_arg("base64String", model_inputs, required=True)
-        format = _parse_arg("format", model_inputs, "mp3")
-        kwargs = _parse_arg("kwargs", model_inputs, {})
-        if format not in ["opus", "wav", "flac", "mp3", "m4a"]:
-            raise Exception(f"Invalid format: {format}")
+    # Access URL from model_inputs dictionary
+    url = model_inputs["url"]
 
-    except Exception as e:
-        return {"error":str(e)}
-    
-    bytes = BytesIO(base64.b64decode(BytesString.encode("ISO-8859-1")))
+    # Access start time from model_inputs dictionary
+    start_time = model_inputs["start_time"]
 
-    tmp_file = "input."+format
-    with open(tmp_file,'wb') as file:
-        file.write(bytes.getbuffer())
-    
+    # Access end time from model_inputs dictionary
+    end_time = model_inputs["end_time"]
+
+    audio_file = "audio-youtube.m4a"
+
+    audio_returncode, audio_stderr = downloadYTaudio(
+        url, start_time, end_time, audio_file
+    )
+
+    if audio_returncode != 0:
+        print(f"Error Audio Download: {audio_stderr}")
+        return audio_stderr
+
+    kwargs = {"beam_size": 5, "temperature": [0, 0.2, 0.4, 0.6, 0.8, 1]}
+
     # Run the model
-    result = model.transcribe(tmp_file, fp16=True, **kwargs)
-    result['segments'] = [{
-        "id":x['id'],
-        "seek":x['seek'],
-        "start":x['start'],
-        "end":x['end'],
-        "text":x['text']
-        } for x in result['segments']]
-    os.remove(tmp_file)
+    result = model.transcribe(audio_file, fp16=True, **kwargs)
+    result["segments"] = [
+        {
+            "id": x["id"],
+            "seek": x["seek"],
+            "start": x["start"],
+            "end": x["end"],
+            "text": x["text"],
+        }
+        for x in result["segments"]
+    ]
+    os.remove(audio_file)
     # Return the results as a dictionary
     return result
--- server.py
+++ server.py
@@ -14,19 +14,21 @@ user_src.init()
 # Create the http server app
 server = Sanic("my_app")
 
+
 # Healthchecks verify that the environment is correct on Banana Serverless
-@server.route('/healthcheck', methods=["GET"])
+@server.route("/healthcheck", methods=["GET"])
 def healthcheck(request):
     # dependency free way to check if GPU is visible
     gpu = False
     out = subprocess.run("nvidia-smi", shell=True)
-    if out.returncode == 0: # success state on shell command
+    if out.returncode == 0:  # success state on shell command
         gpu = True
 
     return response.json({"state": "healthy", "gpu": gpu})
 
+
 # Inference POST handler at '/' is called for every http call from Banana
-@server.route('/', methods=["POST"]) 
+@server.route("/", methods=["POST"])
 def inference(request):
     try:
         model_inputs = response.json.loads(request.json)
@@ -38,5 +40,5 @@ def inference(request):
     return response.json(output)
 
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port="8000", workers=1)
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port="8000", workers=1)
